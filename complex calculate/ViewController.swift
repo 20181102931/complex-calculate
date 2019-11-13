ca4ece459590e6815581eb9734ac5d559fde6563
20181102931 <18647197975@163.com>
2019-11-13 11:51:38 +0800

    112

--- complex calculate/ViewController.swift
+++ complex calculate/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Input: UITextField!
+    @IBAction func Number(_ sender: UIButton) {
+        Input.text = Input.text! + sender.currentTitle!
+    }
+   @IBAction func calculate(_ sender: UIButton) {
+         // Number(_ sender: UIButton)
+    let op = sender.title as! String
+        switch op
+        {
+        case "+":
+            Input.text = "\(Input.text + )"
+        case "-":
+            Input.text = "\(Input.text)"
+        case "*":
+            Input.text = "\(Input.text)"
+        case "/":
+            Input.text = "\(Input.text)"
+        default:
+            break
+        }
+   }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        Input.text = ""
+    }
+    
+    @IBAction func result(_ sender: UIButton) {
+        Input.text
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
