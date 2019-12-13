80e11d3a52b223af6932898583fc16f7795ab384
20181102931 <18647197975@163.com>
2019-12-13 09:47:04 +0800

    123
    23

--- complex calculate/ViewController.swift
+++ complex calculate/ViewController.swift
@@ -7,38 +7,62 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
     @IBOutlet weak var Input: UITextField!
+    var num1=Double(0.0) //保存上一个值
+    var op = ""//当前操作符号
     @IBAction func Number(_ sender: UIButton) {
-        Input.text = Input.text! + sender.currentTitle!
+        if(sender.currentTitle! == ".")  //判断只能输入一次小数点
+        {
+            if(Input.text?.contains("."))!
+            {
+                return
+            }
+        }
+        //输入0.的话直接替换，不是的话正常输入
+        if(Input.text! == "0"&&sender.currentTitle! != ".")
+        {
+            Input.text = sender.currentTitle!
+        }
+        else
+        {
+             Input.text = Input.text! + sender.currentTitle!
+        }
     }
-   @IBAction func calculate(_ sender: UIButton) {
-         // Number(_ sender: UIButton)
-    let op = sender.title as! String
+   @IBAction func calculate(_ sender: UIButton)
+   {
+      op = sender.currentTitle!
+      num1 = Double(Input.text!)!
+      Input.text = "0"
+   }
+    @IBAction func clear(_ sender: UIButton) {
+        Input.text = "0"
+    }
+    
+    //最后结果的计算
+    @IBAction func finalresult(_ sender: UIButton) {
+        
+        let num2 = Double(Input.text!)!
         switch op
         {
         case "+":
-            Input.text = "\(Input.text + )"
+            Input.text = String(num1+num2)
         case "-":
-            Input.text = "\(Input.text)"
+            Input.text = String(num1-num2)
         case "*":
-            Input.text = "\(Input.text)"
+            Input.text = String(num1*num2)
         case "/":
-            Input.text = "\(Input.text)"
+            Input.text = String(num1/num2)
         default:
             break
         }
-   }
-    
-    @IBAction func clear(_ sender: UIButton) {
-        Input.text = ""
+        
     }
     
-    @IBAction func result(_ sender: UIButton) {
-        Input.text
-    }
+    
+    //    @IBAction func result(_ sender: UIButton) {
+//        Input.text
+//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
